cd2c6b7b7a8a95c1734c88244217c5432c9f8ad2
webdesign3r <info@webdesign3r.de>
2017-05-26 14:35:10 +0200

    Initial commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ Voyager::setting('title') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/font-awesome.css') }}" rel="stylesheet">
+</head>
+<body>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,10 @@
+@include('layouts.header')
+
+<div class="container">
+    <div class="col-sm-6"><i class="fa fa-home"></i> asdasdasdaasd</div>
+</div>
+
+
+
+@include('layouts.footer')
+

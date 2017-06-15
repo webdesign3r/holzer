f378b26cc4fb6358b9f57917d09b1cb4638e6ea5
webdesign3r <info@webdesign3r.de>
2017-06-15 18:26:45 +0200

    Revert "installer"
    This reverts commit 30900e1c2e4d0172f437945cbcca687273b5717d.

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        RachidLaasri\LaravelInstaller\Providers\LaravelInstallerServiceProvider::class,
 
         /*
          * Package Service Providers...
